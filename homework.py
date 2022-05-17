5051fe0010c24cec0336f2d8925fed6d3a1ba271
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-05-18 00:14:30 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 
 users = [
-    {"name": "Roman", "birthday": "4 May 2002"},
-    {"name": "Karina", "birthday": "24 March 1995"},
-    {"name": "Tanya", "birthday": "18 February 2000"},
+    {"name": "Roman", "birthday": "19 May 2002"},
+    {"name": "Karina", "birthday": "22 May 1995"},
+    {"name": "Tanya", "birthday": "18 May 2000"},
     {"name": "Alexander", "birthday": "12 March 1990"},
 ]
 
@@ -14,20 +14,22 @@ def get_birthdays_per_week(users):
     for i in users:
         birthday = datetime.strptime(i["birthday"], "%d %B %Y")
         date_b = datetime(start.year, birthday.month, birthday.day)
-        if start >= date_b.date() <= end:
+        if start <= date_b.date() <= end:
             day = date_b.weekday()
             if day == 0:
-                print("Monday:" + i["name"])
+                print("Monday: " + i["name"])
             if day == 1:
-                print("Tuesday:", +i["name"])
+                print("Tuesday: ", +i["name"])
             if day == 2:
-                print("Wednesday:" + i["name"])
+                print("Wednesday: " + i["name"])
             if day == 3:
-                print("Thursday:" + i["name"])
+                print("Thursday: " + i["name"])
             if day == 4:
-                print("Friday:" + i["name"])
+                print("Friday: " + i["name"])
             if day in (5, 6):
-                print('Next Monday:' + i['name'])
+                print('Next Monday: ' + i['name'])
+        else:
+            print(i["name"] + ": Has no birthday in this week")
 
 
 get_birthdays_per_week(users)
